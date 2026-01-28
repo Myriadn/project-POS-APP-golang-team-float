c8d81ccd63c8b54d2ba6137fff5b590f39571dfd
Muhammad Anas <manaska129@gmail.com>
2026-01-28 12:02:29 +0700

    Add Permission and RolePermission entities to migration

--- internal/data/migration.go
+++ internal/data/migration.go
@@ -19,6 +19,8 @@ func Migrate(db *gorm.DB) error {
 		&entity.Order{},
 		&entity.OrderItem{},
 		&entity.OTPCode{},
+		&entity.Permission{},
+        &entity.RolePermisson{},
 	)
 
 	if err != nil {
